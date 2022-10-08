1ae94cd6230888b990abe44658f1d18fd0294aa9
sudddy <suddpro@gmail.com>
2022-10-08 18:01:36 +1300

    Clean up

--- Services/Jobs/index.js
+++ Services/Jobs/index.js
@@ -1,75 +1,67 @@
 var router = require("express").Router();
 var Jobs = require("../../Model/Job/index");
 var jsonParser = require("body-parser").json();
-var mongoose = require("mongoose");
 
+const addJob = async function (req, res, next) {
+  const job = req.body;
+  try {
+    const jobInfo = new Jobs(job);
+    return jobInfo.save(function (err, job) {
+      if (err) res.send(err);
 
+      res.send(job);
+    });
+  } catch (e) {
+    next(e);
+  }
+};
 
-const addJob = async function(req, res, next) {
-    const job = req.body;
-    try {
-      const jobInfo = new Jobs(job);
-      return jobInfo.save(function(err, job) {
+const getJobById = async function (req, res, next) {
+  const id = req.params.id;
+  try {
+    Jobs.find({ _id: id }, function (err, result) {
+      if (err) res.send(err);
+
+      res.send(result);
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const getAllJobs = async function (req, res, next) {
+  try {
+    Jobs.find({}, function (err, result) {
+      if (err) res.send(err);
+
+      res.send(result);
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const updateJob = async function (req, res, next) {
+  const job = req.body;
+  const filter = { _id: job._id };
+  delete job._id;
+  const updateDetails = job;
+
+  try {
+    return Jobs.findOneAndUpdate(
+      filter,
+      updateDetails,
+      { returnOriginal: false },
+      function (err, result) {
         if (err) res.send(err);
-  
-        res.send(job);
-      });
-    } catch (e) {
-      next(e);
-    }
-  };
-  
 
-  const getJobById = async function(req, res, next) {
-    let id = req.params.id;
-    try {
-        Jobs.find({ _id: id }, function(err, result) {
-        if (err) res.send(500);
-  
         res.send(result);
-      });
-    } catch (e) {
-      next(e);
-    }
-  };
-
-  const getAllJobs = async function(req, res, next) {
-    try {
-        Jobs.find({}, function(err, result) {
-        if (err) res.send(500);
-  
-        res.send(result);
-      });
-    } catch (e) {
-      next(e);
-    }
-  };
-
-
-  const updateJob = async function(req, res, next) {
-    const job = req.body;
-    const filter = { _id: job._id };
-    delete job._id;
-    const updateDetails = job;
-    console.log(updateDetails)
-    try {
-      await Jobs.findOneAndUpdate(
-        filter,
-        { $set: updateDetails },
-        { returnOriginal: false },
-        function(err, result) {
-          if (err) res.send(response.dbError());
-  
-          res.send(result);
-        }
-      );
-    } catch (e) {
-      next(e);
-    }
-  };
-
-
-
+      }
+    );
+  } catch (e) {
+    next(e);
+  }
+};
 
 router.get("/jobs/:id", getJobById);
 
@@ -77,7 +69,6 @@ router.post("/jobs", jsonParser, addJob);
 
 router.put("/jobs", jsonParser, updateJob);
 
-router.get("/jobs", jsonParser,getAllJobs);
-
+router.get("/jobs", jsonParser, getAllJobs);
 
 module.exports = router;
